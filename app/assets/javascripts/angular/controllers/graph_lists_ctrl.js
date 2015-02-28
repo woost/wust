9027e64e6452dc78073c4d6d39f2eb6a7ab1dfdf
johannes karoff <johannes@karoff.net>
2015-02-28 21:17:30 +0100

    search box for filtering in problem browser

--- app/assets/javascripts/angular/controllers/graph_lists_ctrl.js
+++ app/assets/javascripts/angular/controllers/graph_lists_ctrl.js
@@ -1,6 +1,15 @@
-app.controller('GraphListsCtrl', function($scope, $state, initialData) {
-    $scope.graph = initialData;
+app.controller('GraphListsCtrl', function($scope, $state, $filter, initialData) {
+    var nodes = new vis.DataSet();
+    var edges = new vis.DataSet();
+    nodes.add(initialData.nodes);
+    edges.add(initialData.edges);
+    $scope.graph = {
+        nodes: nodes,
+        edges: edges
+    };
+
     $scope.onSelect = onSelect;
+    $scope.$watch('search.label', filter);
 
     function onSelect(properties) {
         var id = properties.nodes[0];
@@ -8,6 +17,14 @@ app.controller('GraphListsCtrl', function($scope, $state, initialData) {
             return;
         }
 
-        $state.go('graphs.detail', { id: id });
+        $state.go('graphs.detail', {
+            id: id
+        });
+    }
+
+    function filter() {
+        var filtered = $filter('filter')(initialData.nodes, $scope.search);
+        nodes.clear();
+        nodes.add(filtered);
     }
 });
--- app/assets/javascripts/angular/directives/vis_network.js
+++ app/assets/javascripts/angular/directives/vis_network.js
@@ -8,22 +8,11 @@ app.directive('visNetwork', function() {
             options: '='
         },
         link: function($scope, $element, $attrs, ngModel) {
-            var nodes = new vis.DataSet();
-            var edges = new vis.DataSet();
-            nodes.add($scope.ngModel.nodes);
-            edges.add($scope.ngModel.edges);
-            var graph = {
-                nodes: nodes,
-                edges: edges
-            };
-
-            var network = new vis.Network($element[0], graph, $scope.options || {});
+            var network = new vis.Network($element[0], $scope.ngModel, $scope.options || {});
             var onSelect = $scope.onSelect() || function(prop) {};
             network.on('select', function(properties) {
                 onSelect(properties);
             });
-
         }
-
-    }
+    };
 });
