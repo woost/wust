30868d15964d4d14767431981d42a6614e184a72
johannes karoff <johannes@karoff.net>
2015-03-07 18:42:02 +0100

    refine problem page

--- app/assets/javascripts/controllers/problem_views_ctrl.js
+++ app/assets/javascripts/controllers/problem_views_ctrl.js
@@ -1,43 +1,17 @@
 app.controller('ProblemViewsCtrl', function($scope, $state, $stateParams, Problem) {
-    var newIdea = {
+    $scope.ideas = Problem.queryIdeas($stateParams.id);
+
+    $scope.addIdea = addIdea;
+    $scope.removeIdea = undefined;
+    $scope.newIdea = {
         title: ""
     };
-    var ideas = Problem.queryIdeas($stateParams.id);
-    $scope.ideas = {
-        new: newIdea,
-        list: ideas,
-        add: addConnected(Problem.createIdea, ideas, newIdea),
-        remove: functionToDo,
-        click: gotoIdea
-    };
 
-    var newGoal = {
-        title: ""
-    };
-    var goals = Problem.queryGoals($stateParams.id);
-    $scope.goals = {
-        new: newGoal,
-        list: goals,
-        add: addConnected(Problem.createGoal, goals, newGoal),
-        remove: functionToDo,
-        click: functionToDo
-    };
-
-    function gotoIdea(idea) {
-        $state.go("problems.idea({id: $stateParams.id, ideaId: idea.id})");
-    }
-
-    function addConnected(createFunction, itemList, newItem) {
-        return function() {
-            createFunction($stateParams.id, newItem).$promise.then(function(data) {
-                toastr.success("Added new Item");
-                itemList.push(data);
-                newItem.title = "";
-            });
-        };
-    }
-
-    function functionToDo(something) {
-        toastr.error("Hi, I am a function to be implemented");
+    function addIdea() {
+        Problem.createIdea($stateParams.id, $scope.newIdea).$promise.then(function(data) {
+            toastr.success("Added new idea");
+            $scope.ideas.push(data);
+            $scope.newIdea.title = "";
+        });
     }
 });
--- app/assets/javascripts/controllers/problems_ctrl.js
+++ app/assets/javascripts/controllers/problems_ctrl.js
@@ -1,3 +1,17 @@
 app.controller('ProblemsCtrl', function($scope, $stateParams, Problem) {
     $scope.problem = Problem.get($stateParams.id);
+    $scope.goals = Problem.queryGoals($stateParams.id);
+
+    $scope.addGoal = addGoal;
+    $scope.newGoal = {
+        title: ""
+    };
+
+    function addGoal() {
+        Problem.createGoal($stateParams.id, $scope.newGoal).$promise.then(function(data) {
+            toastr.success("Added new goal");
+            $scope.goals.push(data);
+            $scope.newGoal.title = "";
+        });
+    }
 });
