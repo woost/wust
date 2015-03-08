847d4378bd60d9f006ec99341fff97daa09452e3
Felix Dietze <felix@beerleader.net>
2015-03-08 17:46:39 +0100

    interactive vote mock

--- app/assets/javascripts/controllers/votes_ctrl.js
+++ app/assets/javascripts/controllers/votes_ctrl.js
@@ -18,49 +18,70 @@ app.controller('VotesCtrl', function($scope){
             }
         }
     },
-    {
-        action: 'connect',
-        icon: 'fa-compress',
-        affected: {
-            startnode: {
-                title: 'Ich ernähre mich schlecht',
-                class: 'discourse_problem'
-            },
-            relation: {
-                title: 'causes'
-            },
-            endnode: {
-                title: 'Ich bin zu dick',
-                class: 'discourse_problem'
+        {
+            action: 'connect',
+            icon: 'fa-compress',
+            affected: {
+                startnode: {
+                    title: 'Ich ernähre mich schlecht',
+                    class: 'discourse_problem'
+                },
+                relation: {
+                    title: 'causes'
+                },
+                endnode: {
+                    title: 'Ich bin zu dick',
+                    class: 'discourse_problem'
+                }
             }
-        }
-    },
-    {
-        action: 'disconnect',
-        icon: 'fa-expand',
-        affected: {
-            startnode: {
-                title: 'PC neustarten',
-                class: 'discourse_idea'
-            },
-            relation: {
-                title: 'solves'
-            },
-            endnode: {
-                title: 'Ich ernähre mich schlecht',
-                class: 'discourse_problem'
+        },
+        {
+            action: 'disconnect',
+            icon: 'fa-expand',
+            affected: {
+                startnode: {
+                    title: 'PC neustarten',
+                    class: 'discourse_idea'
+                },
+                relation: {
+                    title: 'solves'
+                },
+                endnode: {
+                    title: 'Ich ernähre mich schlecht',
+                    class: 'discourse_problem'
+                }
             }
-        }
-    },
-    {
-        action: 'flag',
-        icon: 'fa-flag',
-        affected: {
-            node: {
-                title: 'Buy Viagra!'
+        },
+            {
+                action: 'flag',
+                icon: 'fa-flag',
+                affected: {
+                    node: {
+                        title: 'Buy Viagra!'
+                    },
+                    flag: 'spam'
+                }
             },
-        }
-    },
-    ];
+            ];
+
+    $scope.changeindex = 0;
+    $scope.change = $scope.changes[$scope.changeindex];
+    $scope.showundo = false;
+
+    $scope.support = skip;
+    $scope.oppose = skip;
+    $scope.skip = skip;
+    $scope.undo = undo;
+
+    function skip() {
+        $scope.changeindex = ($scope.changeindex + 1) % $scope.changes.length;
+        $scope.change = $scope.changes[$scope.changeindex];
+        $scope.showundo = true;
+    }
+
+    function undo() {
+        $scope.changeindex = ($scope.changes.length + $scope.changeindex - 1) % $scope.changes.length;
+        $scope.change = $scope.changes[$scope.changeindex];
+    }
 
 });
