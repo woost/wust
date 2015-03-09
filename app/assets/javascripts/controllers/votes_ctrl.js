fdc9f44631fb4486d7040c2fd3959f96d87447e1
Felix Dietze <felix@beerleader.net>
2015-03-09 15:54:17 +0100

    colored discourse node panel

--- app/assets/javascripts/controllers/votes_ctrl.js
+++ app/assets/javascripts/controllers/votes_ctrl.js
@@ -1,68 +1,65 @@
-app.controller('VotesCtrl', function($scope){
-    $scope.changes = [
-    {
+app.controller('VotesCtrl', function($scope) {
+    $scope.changes = [{
         action: 'delete',
         icon: 'fa-trash-o',
         affected: {
             node: {
-                title: 'Du bist doof'
+                title: 'Du bist doof',
+                class: 'discourse_goal node'
             }
         }
-    },
-    {
+    }, {
         action: 'create',
         icon: 'fa-star-o',
         affected: {
             node: {
-                title: 'Ich bin zu dick'
+                title: 'Ich bin zu dick',
+                class: 'discourse_problem node'
             }
         }
-    },
-        {
-            action: 'connect',
-            icon: 'fa-compress',
-            affected: {
-                startnode: {
-                    title: 'Ich ernähre mich schlecht',
-                    class: 'discourse_problem'
-                },
-                relation: {
-                    title: 'causes'
-                },
-                endnode: {
-                    title: 'Ich bin zu dick',
-                    class: 'discourse_problem'
-                }
-            }
-        },
-        {
-            action: 'disconnect',
-            icon: 'fa-expand',
-            affected: {
-                startnode: {
-                    title: 'PC neustarten',
-                    class: 'discourse_idea'
-                },
-                relation: {
-                    title: 'solves'
-                },
-                endnode: {
-                    title: 'Ich ernähre mich schlecht',
-                    class: 'discourse_problem'
-                }
-            }
-        },
-            {
-                action: 'flag',
-                icon: 'fa-flag',
-                affected: {
-                    node: {
-                        title: 'Buy Viagra!'
-                    },
-                    flag: 'spam'
-                }
+    }, {
+        action: 'connect',
+        icon: 'fa-compress',
+        affected: {
+            startnode: {
+                title: 'Ich ernähre mich schlecht',
+                class: 'discourse_problem node'
             },
-            ];
+            relation: {
+                title: 'causes'
+            },
+            endnode: {
+                title: 'Ich bin zu dick',
+                class: 'discourse_problem node'
+            }
+        }
+    }, {
+        action: 'disconnect',
+        icon: 'fa-expand',
+        affected: {
+            startnode: {
+                title: 'PC neustarten',
+                class: 'discourse_idea node'
+            },
+            relation: {
+                title: 'solves'
+            },
+            endnode: {
+                title: 'Ich ernähre mich schlecht',
+                class: 'discourse_problem node'
+            }
+        }
+    }, {
+        action: 'flag',
+        icon: 'fa-flag',
+        affected: {
+            node: {
+                title: 'Buy Viagra!',
+                class: 'discourse_goal node'
+            },
+            flag: 'spam'
+        }
+    }, ];
 
     $scope.changeindex = 0;
     $scope.change = $scope.changes[$scope.changeindex];
