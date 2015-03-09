df2691dd9eab01ecf16865006e90ad9b24f5445b
johannes karoff <johannes@karoff.net>
2015-03-09 13:28:59 +0100

    add workaround for issue with ng-animate and bootstrap carousel

---
+++ app/assets/javascripts/services/set_ng_animate.js
@@ -0,0 +1,15 @@
+// workaround for interference between ng-animate and bootstrap carousel
+// individually disable/enable ng-animate for elements
+// https://github.com/angular-ui/bootstrap/issues/1273
+app.directive('setNgAnimate', function ($animate) {
+    return {
+        link: function ($scope, $element, $attrs) {
+            $scope.$watch( function() {
+                return $scope.$eval($attrs.setNgAnimate, $scope);
+            }, function(valnew, valold){
+                console.log('Directive animation Enabled: ' + valnew);
+                $animate.enabled(!!valnew, $element);
+            });
+        }
+    };
+});
