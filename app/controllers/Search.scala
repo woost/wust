b153749912faa79973e55b41fa62c6a83f010230
Felix Dietze <felix@beerleader.net>
2015-03-09 21:10:14 +0100

    limit search results to 15

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -10,7 +10,7 @@ import renesca.parameter.implicits._
 object Search extends Controller with DatabaseController {
   def index(term: String) = Action {
     val regexTerm = ".*" + term.replace(" ", ".*") + ".*"
-    val discourse = Discourse(db.queryGraph(Query("match (n) where n.title =~ {term} return n", Map("term" -> regexTerm))))
+    val discourse = Discourse(db.queryGraph(Query("match (n) where n.title =~ {term} return n limit 15", Map("term" -> regexTerm))))
 
     Ok(Json.toJson(discourse.nodes))
   }
