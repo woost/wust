3ace7202eddfdd1fb664d1f394df336ad85278ac
johannes karoff <johannes@karoff.net>
2015-03-13 00:44:05 +0100

    drag and drop of nodes only applicable for discourse node list of same type

--- app/assets/javascripts/services/discourse_node.js
+++ app/assets/javascripts/services/discourse_node.js
@@ -2,28 +2,30 @@ app.service('DiscourseNode', function() {
     this.goal = {
         css: "discourse_goal",
         state: "goals",
-        color: "#6DFFB4"
+        color: "#6DFFB4",
+        label: "GOAL"
     };
 
     this.problem = {
         css: "discourse_problem",
         state: "problems",
-        color: "#FFDC6D"
+        color: "#FFDC6D",
+        label: "PROBLEM"
     };
 
     this.idea = {
         css: "discourse_idea",
         state: "ideas",
         color: "#6DB5FF",
+        label: "IDEA"
     };
 
     this.get = get;
 
-    var mappings = {
-        GOAL: this.goal,
-        PROBLEM: this.problem,
-        IDEA: this.idea,
-    };
+    var mappings = {};
+    mappings[this.goal.label] = this.goal;
+    mappings[this.problem.label] = this.problem;
+    mappings[this.idea.label] = this.idea;
 
     function get(label) {
         return mappings[label];
