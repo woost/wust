a41c8d5b4311a1eaab094adc8e479c5e3fe04dc6
johannes karoff <johannes@karoff.net>
2015-03-22 19:46:08 +0100

    set dimensions of svg foreignobject according to the size of the inner html content

--- app/assets/javascripts/directives/d3_graph.js
+++ app/assets/javascripts/directives/d3_graph.js
@@ -9,12 +9,16 @@ angular.module("wust").directive("d3Graph", function(DiscourseNode) {
         link: function(scope, element) {
             var onDoubleClick = scope.onClick() || _.noop;
 
+            // watch for changes in the ngModel
             scope.$watchCollection("ngModel", () => {
+                // get current graph
                 var graph = scope.ngModel;
 
+                // get dimensions
                 var width = element[0].offsetWidth;
                 var height = element[0].offsetHeight;
 
+                // force configuration
                 var force = d3.layout.force()
                     .friction(0.90)
                     // .gravity(0.05)
@@ -22,6 +26,7 @@ angular.module("wust").directive("d3Graph", function(DiscourseNode) {
                     .linkDistance(120)
                     .size([width, height]);
 
+                // remove any previous svg
                 d3.select("svg").remove();
 
                 // define events
@@ -30,11 +35,11 @@ angular.module("wust").directive("d3Graph", function(DiscourseNode) {
                     .on("dragstart", dragstarted)
                     .on("drag", dragged);
 
+                // construct svg
                 var svg = d3.select(element[0])
                     .append("svg")
                     .attr("width", width)
                     .attr("height", height)
-                    // .append("g")
                     .call(zoom)
                     .on("dblclick.zoom", null);
 
@@ -51,63 +56,73 @@ angular.module("wust").directive("d3Graph", function(DiscourseNode) {
                     .attr("d", "M0,-5L10,0L0,5")
                     .attr("fill", "red");
 
-
+                // container with enabled pointer events
                 var container = svg.append("g")
                     .style("pointer-events", "all");
 
-
+                // add nodes and edges
                 force
                     .nodes(graph.nodes)
                     .links(graph.edges)
                     .start();
 
-
+                // create edges in the svg container
                 var link = container.selectAll(".svglink")
-                    .data(graph.edges)
-                    .enter().append("line")
+                    .data(graph.edges).enter()
+                    .append("line")
                     .attr("class", "svglink")
                     .style("stroke-width", 1)
                     .style("stroke", "#999")
                     .style("marker-end", "url(#arrow)");
 
-                var linktextSvg = container.append("g").selectAll("g.linklabelholder")
-                    .data(graph.edges)
-                    .enter().append("g");
+                var linktextSvg = container.selectAll("g.linklabelholder")
+                    .data(graph.edges).enter()
+                    .append("g");
 
-                var linktextHtml = linktextSvg
-                    .append("foreignObject")
-                    .attr("width", 600)
-                    .attr("height", 600)
-                    .style("text-align", "center")
-                    .append("xhtml:span")
-                    .style("line-height", "600px")
+                var linktextFo = linktextSvg.append("foreignObject")
+                    .style("text-align", "center");
+
+                var linktextHtml = linktextFo.append("xhtml:span")
                     // .style("text-shadow", "white -1px 0px, white 0px 1px, white 1px 0px, white 0px -1px")
                     .style("background", "white")
-                    .style("padding", "1px")
                     .html(d => d.label);
 
+                setForeignObjectDimensions(linktextFo, linktextHtml);
 
-
-                var node = container.append("g").selectAll(".svgnode")
+                // create nodes in the svg container
+                var node = container.append("g")
+                    .selectAll(".svgnode")
                     .data(graph.nodes)
                     .enter().append("g")
                     .call(drag)
                     .on("click", clicked)
                     .on("dblclick", onDoubleClick);
 
-                var nodeHtml = node.append("foreignObject")
-                    .attr("width", 600)
-                    .attr("height", 600)
-                    .style("text-align", "center")
-                    // .style("opacity", "0.5")
-                    .append("xhtml:div")
-                    .style("margin-top", "290px")
+                var nodeFo = node.append("foreignObject")
+                    .style("opacity", "0.8")
+                    .style("text-align", "center");
+
+                var nodeHtml = nodeFo.append("xhtml:div")
                     .style("max-width", "150px")
                     .style("cursor", "move")
                     .attr("class", d => "node " + DiscourseNode.get(d.label).css)
                     .html(d => d.title);
 
+                setForeignObjectDimensions(nodeFo, nodeHtml);
+
+                // register tick function
                 force.on("tick", tick);
+
+                // we need to set the height and weight of the foreignobject
+                // to the dimensions of the inner html container.
+                function setForeignObjectDimensions(fo, html) {
+                    for (var i = 0; i < fo[0].length; i++) {
+                        var rect = html[0][i].getBoundingClientRect();
+                        var curr = fo[0][i];
+                        curr.setAttribute("width", rect.width);
+                        curr.setAttribute("height", rect.height);
+                    }
+                }
 
                 function tick() {
                     link
@@ -117,10 +132,13 @@ angular.module("wust").directive("d3Graph", function(DiscourseNode) {
                         .attr("y2", d => d.target.y);
 
                     linktextSvg
-                        .attr("transform", d => "translate(" + ((d.source.x + d.target.x) / 2 - 300) + "," + ((d.source.y + d.target.y) / 2 - 300) + ")");
+                        .attr("transform", d => "translate(" + ((d.source.x + d.target.x) / 2) + "," + ((d.source.y + d.target.y) / 2) + ")");
 
-
-                    node.attr("transform", d => "translate(" + (d.x - 300) + "," + (d.y - 300) + ")");
+                    node.attr("transform", d => {
+                        // center the node
+                        var rect = nodeHtml[0][d.index].getBoundingClientRect();
+                        return "translate(" + (d.x - rect.width / 2) + "," + (d.y - rect.height / 2) + ")";
+                    });
                 }
 
                 function zoomed() {
@@ -135,9 +153,6 @@ angular.module("wust").directive("d3Graph", function(DiscourseNode) {
                 }
 
                 function setFixedPosition(d) {
-                    // prevent d3 from interpreting this as panning
-                    d3.event.sourceEvent.stopPropagation();
-
                     d3.select(this).classed("fixed", d.fixed = true);
                 }
 
@@ -145,8 +160,11 @@ angular.module("wust").directive("d3Graph", function(DiscourseNode) {
                 var isDragging = false;
 
                 function clicked(d) {
-                    // do not interpret this as a click if we dragged earlier.
-                    if (!isDragging) {
+                    if (isDragging) {
+                        // if we were dragging before, the node should be fixed
+                        setFixedPosition(d);
+                    } else {
+                        // if the user just clicked, the position should be reset.
                         // we wait a moment before unsetting the fixed position in
                         // case the user wants to double click the node, so it does
                         // not float away beforehand.
@@ -157,7 +175,8 @@ angular.module("wust").directive("d3Graph", function(DiscourseNode) {
                 }
 
                 function dragstarted(d) {
-                    setFixedPosition(d);
+                    // prevent d3 from interpreting this as panning
+                    d3.event.sourceEvent.stopPropagation();
                 }
 
                 function dragged(d) {
