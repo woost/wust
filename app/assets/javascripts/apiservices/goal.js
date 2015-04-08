8369b8d816695ce2721ef56f4ac828853831eefd
johannes karoff <johannes@karoff.net>
2015-04-09 00:54:13 +0200

    no base url for nested models

--- app/assets/javascripts/apiservices/goal.js
+++ app/assets/javascripts/apiservices/goal.js
@@ -1,7 +1,7 @@
 angular.module("wust").factory("Goal", function(restmod) {
     return restmod.model("/goals").mix({
-        goals: { hasMany: restmod.model("/goals") },
-        problems: { hasMany: restmod.model("/problems") },
-        ideas: { hasMany: restmod.model("/ideas") },
+        goals: { hasMany: restmod.model() },
+        problems: { hasMany: restmod.model() },
+        ideas: { hasMany: restmod.model() },
     });
 });
--- app/assets/javascripts/apiservices/idea.js
+++ app/assets/javascripts/apiservices/idea.js
@@ -1,7 +1,7 @@
 angular.module("wust").factory("Idea", function(restmod) {
     return restmod.model("/ideas").mix({
-        goals: { hasMany: restmod.model("/goals") },
-        problems: { hasMany: restmod.model("/problems") },
-        ideas: { hasMany: restmod.model("/ideas") },
+        goals: { hasMany: restmod.model() },
+        problems: { hasMany: restmod.model() },
+        ideas: { hasMany: restmod.model() },
     });
 });
