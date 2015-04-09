3d7ef34e57c0b6cb51dd0e36a24e002b2c251128
johannes karoff <johannes@karoff.net>
2015-04-09 18:32:48 +0200

    fix errors in fuzzy filter

--- app/assets/javascripts/filters/fuzzy_filter.js
+++ app/assets/javascripts/filters/fuzzy_filter.js
@@ -2,6 +2,9 @@ angular.module("wust").filter("fuzzyFilter", ["$filter",
     function($filter) {
         return (data, obj) => {
             _.forIn(obj, (value, key) => {
+                if (!_.isString(value) && !_.isNumber(value))
+                    return;
+
                 _.each(value.split(" "), (val) => {
                     var search = {};
                     search[key] = val || "";
