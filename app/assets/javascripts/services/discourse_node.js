823d8f15383e367ae8dc821c3617fca049ed5642
johannes karoff <johannes@karoff.net>
2015-04-10 02:07:23 +0200

    remove unneeded color definition from DiscourseNode service

--- app/assets/javascripts/services/discourse_node.js
+++ app/assets/javascripts/services/discourse_node.js
@@ -2,21 +2,18 @@ angular.module("wust").service("DiscourseNode", function() {
     this.goal = {
         css: "discourse_goal",
         state: "goals",
-        color: "#6DFFB4",
         label: "GOAL"
     };
 
     this.problem = {
         css: "discourse_problem",
         state: "problems",
-        color: "#FFDC6D",
         label: "PROBLEM"
     };
 
     this.idea = {
         css: "discourse_idea",
         state: "ideas",
-        color: "#6DB5FF",
         label: "IDEA"
     };
 
