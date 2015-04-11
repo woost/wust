1f6722d833d7e5017dc253002e320c05d09e0ccc
johannes karoff <johannes@karoff.net>
2015-04-11 12:42:29 +0200

    add button for registering new users

--- app/assets/javascripts/controllers/navigation_ctrl.js
+++ app/assets/javascripts/controllers/navigation_ctrl.js
@@ -3,6 +3,7 @@ angular.module("wust").controller("NavigationCtrl", function($scope, Auth, Searc
         title: ""
     };
 
+    $scope.registerUser = false;
     $scope.newUser = {
         identifier: "",
         password: ""
@@ -10,10 +11,14 @@ angular.module("wust").controller("NavigationCtrl", function($scope, Auth, Searc
 
     $scope.searchNodes = searchNodes;
     $scope.onSelect = onSelect;
-    $scope.login = Auth.login.bind(Auth, $scope.newUser);
-    $scope.register = Auth.register.bind(Auth, $scope.newUser);
+    $scope.authenticate = authenticate;
     $scope.loggedIn = Auth.loggedIn.bind(Auth);
     $scope.logout = Auth.logout.bind(Auth);
+
+    function authenticate() {
+        let func = $scope.registerUser ? Auth.register : Auth.login;
+        return func.bind(Auth)($scope.newUser);
+    }
 
     function searchNodes(title) {
         return Search.$search({title: title});
