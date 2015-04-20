7ea710b6ab3b7966956c7d0ad5449a3bd18920f4
johannes karoff <johannes@karoff.net>
2015-04-20 19:24:18 +0200

    rename discourse node list directive

--- app/assets/javascripts/directives/discourse_node_list.js
+++ app/assets/javascripts/directives/discourse_node_list.js
@@ -1,4 +1,4 @@
-angular.module("wust").directive("showDiscourseNodeList", function() {
+angular.module("wust").directive("discourseNodeList", function() {
     return {
         restrict: "A",
         require: "ngModel",
