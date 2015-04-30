d42bc3c156b642d686755b52f6cdf8390c7c386b
johannes karoff <johannes@karoff.net>
2015-04-30 22:27:38 +0200

    add script to automatically create component scaffold in js (controller, html file, scss file)

---
+++ app/assets/app/components/create_component.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage: `basename $0` <component>"
+    exit -1
+fi
+
+##################################################
+
+component=$1
+component_plural="${component}s"
+
+controller_name="${component_plural^}Ctrl"
+controller_file="${component_plural}_ctrl.js"
+html_id="${component}_container"
+html_file="${component}.html"
+scss_file="_${component}.scss"
+
+success="\e[0;32m✔\e[0m"
+failure="\e[0;31m✗\e[0m"
+bold=`tput bold`
+normal=`tput sgr0`
+
+#################################################
+
+chk_ret(){
+    if [ $1 -eq 0 ]; then
+        echo -e "  $success $2"
+    else
+        echo -e "  $failure $2"
+        exit 5
+    fi
+}
+
+echo -e "${bold}Setup component '$component'${normal}"
+mkdir $component_plural
+chk_ret $? "make component directory"
+cd $component_plural
+
+controller_def=$(
+cat << EOF
+angular.module("wust.components").controller("$controller_name", function(\$scope) {
+    
+})
+EOF
+)
+echo "$controller_def" > $controller_file
+chk_ret $? "write controller file"
+
+html_def=$(
+cat << EOF
+<div id="$html_id">
+    
+</div>
+EOF
+)
+echo "$html_def" > $html_file
+chk_ret $? "write html file"
+
+scss_def=$(
+cat << EOF
+#$html_id {
+    
+}
+EOF
+)
+echo "$scss_def" > $scss_file
+chk_ret $? "write scss file"
+
+#routes_def
+echo
+echo -e "${bold}Routes${normal}"
+cat << EOF
+.state("${component_plural}", {
+        parent: "page",
+        url: "${component_plural}",
+        templateUrl: \`\${templateBase}/${component_plural}/${component}.html\`,
+        controller: "$controller_name"
+})
+EOF
