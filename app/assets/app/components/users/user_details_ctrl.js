52f39760cd7103ee26450a9f99e53bfa152db47e
johannes karoff <johannes@karoff.net>
2015-05-19 01:49:15 +0200

    subscribe to contributes events on user page

--- app/assets/app/components/users/user_details_ctrl.js
+++ app/assets/app/components/users/user_details_ctrl.js
@@ -1,11 +1,16 @@
 angular.module("wust.components").controller("UserDetailsCtrl", UserDetailsCtrl);
 
-UserDetailsCtrl.$inject = ["$stateParams", "User", "DiscourseNodeList"];
+UserDetailsCtrl.$inject = ["$rootScope", "$stateParams", "User", "DiscourseNodeList"];
 
-function UserDetailsCtrl($stateParams, User, DiscourseNodeList) {
+function UserDetailsCtrl($rootScope, $stateParams, User, DiscourseNodeList) {
     let vm = this;
 
     vm.user = User.$find($stateParams.id);
 
     vm.contributions = DiscourseNodeList.Any(vm.user.contributes, "Contributions");
+    let unsubscribe = vm.contributions.subscribe();
+    let deregisterEvent = $rootScope.$on("$stateChangeSuccess", () => {
+        unsubscribe();
+        deregisterEvent();
+    });
 }
