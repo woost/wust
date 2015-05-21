ec92113edc04e153f11585426ddaba9dbda32336
johannes karoff <johannes@karoff.net>
2015-05-21 17:21:52 +0200

    correct direction of api for untypeds

--- app/controllers/api/Untypeds.scala
+++ app/controllers/api/Untypeds.scala
@@ -8,7 +8,7 @@ import modules.requests._
 //TODO nodeschema should have type NodeSchema[Untyped] not ContentNode...
 object Untypeds extends Nodes[ContentNode] {
   lazy val nodeSchema = NodeSchema(routePath, new ContentNodeAccess(Untyped), Map(
-    "responds" -> StartConnectSchema(new StartContentRelationAccess(Refers, Untyped)),
-    "prays" -> EndConnectSchema(new EndContentRelationAccess(Refers, Untyped))
+    "responds" -> EndConnectSchema(new EndContentRelationAccess(Refers, Untyped)),
+    "prays" -> StartConnectSchema(new StartContentRelationAccess(Refers, Untyped))
   ))
 }
