4790a4a975b0a61da001366924b2ef626ec323d9
johannes karoff <johannes@karoff.net>
2015-05-23 23:19:38 +0200

    fix edit of title/description on focus_view

--- app/assets/app/components/nodes/focus_view.js
+++ app/assets/app/components/nodes/focus_view.js
@@ -13,31 +13,34 @@ function focusView($state, $rootScope, $q, NodeHistory) {
     function link($scope) {
         let vm = $scope.vm;
 
+        // callbacks for removing/updating the focused node
+        vm.removeFocused = removeFocused;
+        vm.updateFocused = updateFocused;
+
+        // we are visiting the focus view of this node, thus the node is added to the NodeHistory
         vm.node.model.$then(data => {
             NodeHistory.add(data);
         });
-
-        // callbacks for removing/updating the focused node
-        vm.removeFocused = _.wrap(vm.node.model, removeFocused);
-        vm.updateFocused = _.wrap(vm.node.model, updateFocused);
 
         // register for events for the current node, as well as all connected lists
         vm.node.subscribe();
         _.each(_.compact([vm.left, vm.right, vm.bottom, vm.top]), list => list.subscribe());
 
-    }
+        function removeFocused() {
+            vm.node.model.$destroy().$then(() => {
+                NodeHistory.remove(vm.node.model.id);
+                humane.success("Removed node");
+                $state.go("browse");
+            });
+        }
 
-    function removeFocused(node) {
-        node.$destroy().$then(() => {
-            NodeHistory.remove(node.id);
-            humane.success("Removed node");
-            $state.go("browse");
-        });
-    }
-
-    function updateFocused(node) {
-        return node.$save().$then(() => {
-            humane.success("Updated node");
-        }).$promise;
+        function updateFocused(field, data) {
+            let node = angular.copy(vm.node.model).$extend({
+                [field]: data
+            });
+            return node.$save().$then(() => {
+                humane.success("Updated node");
+            }).$promise;
+        }
     }
 }
