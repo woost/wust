950baa9182a37708dfe3a376059537758d5fad2e
johannes karoff <johannes@karoff.net>
2015-05-30 14:51:44 +0200

    add route loading indicator

---
+++ app/assets/app/wust/directives/route_loading_indicator.js
@@ -0,0 +1,30 @@
+angular.module("wust").directive("routeLoadingIndicator", routeLoadingIndicator);
+
+routeLoadingIndicator.$inject = ["$rootScope"];
+
+function routeLoadingIndicator($rootScope) {
+    return {
+        restrict: "A",
+        template: "<div class='route_loading_indicator'></div>",
+        replace: true,
+        link: function(scope, elem, attrs) {
+            let lodium = new Lodium(elem[0]);
+            scope.isRouteLoading = false;
+
+            $rootScope.$on("$stateChangeStart", () => {
+                scope.isRouteLoading = true;
+                lodium.start();
+            });
+
+            $rootScope.$on("$stateChangeError", () => {
+                scope.isRouteLoading = false;
+                lodium.stop();
+            });
+
+            $rootScope.$on("$stateChangeSuccess", () => {
+                scope.isRouteLoading = false;
+                lodium.stop();
+            });
+        }
+    };
+}
