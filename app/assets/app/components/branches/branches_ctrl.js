5531f4fb92bb8aa843257a054b06b3b54b9e8335
Felix Dietze <felix@beerleader.net>
2015-06-02 23:25:55 +0200

    sorting support in discourse_node_list

--- app/assets/app/components/branches/branches_ctrl.js
+++ app/assets/app/components/branches/branches_ctrl.js
@@ -7,14 +7,16 @@ function BranchesCtrl(ConnectedComponents, $stateParams, DiscourseNodeList) {
 
     vm.component = ConnectedComponents.$find($stateParams.id).$then(data => {
         vm.nodeList = DiscourseNodeList.read(data.nodes);
+        vm.nodeList.model.orderProperty = "line";
     });
 
     vm.rootId = $stateParams.id;
 
     vm.onGraphDraw = () => {
+        console.log(2);
         // sort node list by according to node position in branch
-        let idToGraphNode = _.indexBy(vm.component.nodes, "id");
-        vm.nodeList.model.list = _.sortBy(vm.nodeList.model.list, n => idToGraphNode[n.id].line);
+        // let idToGraphNode = _.indexBy(vm.component.nodes, "id");
+        // vm.nodeList.model.list = _.sortBy(vm.nodeList.model.list, n => idToGraphNode[n.id].line);
 
         // TODO: reposition nodes in branch
         // at this point in time angular didn't render the nodes yet
---
+++ app/assets/app/wust/filters/conditional_order_by.js
@@ -0,0 +1,10 @@
+angular.module("wust").filter("conditionalOrderBy", conditionalOrderBy);
+
+conditionalOrderBy.$inject = ["$filter"];
+
+function conditionalOrderBy($filter) {
+    return function(xs, expression, reverse) {
+        if (expression === undefined) return xs;
+        return $filter("orderBy")(xs, expression, reverse);
+    };
+}
