35cb883a52c80977626d45a490f6f503b7345a6c
johannes karoff <johannes@karoff.net>
2015-06-02 17:10:53 +0200

    automatically hide search results when the route changes

--- app/assets/app/components/search/search_ctrl.js
+++ app/assets/app/components/search/search_ctrl.js
@@ -1,10 +1,14 @@
 angular.module("wust.components").controller("SearchCtrl", SearchCtrl);
 
-SearchCtrl.$inject = ["SearchService", "DiscourseNode"];
+SearchCtrl.$inject = ["SearchService", "DiscourseNode", "$rootScope"];
 
-function SearchCtrl(SearchService, DiscourseNode) {
+function SearchCtrl(SearchService, DiscourseNode, $rootScope) {
     let vm = this;
 
     vm.search = SearchService.search;
     vm.nodeInfo = DiscourseNode.Post;
+
+    $rootScope.$on("$stateChangeSuccess", () => {
+        vm.search.resultsVisible = false;
+    });
 }
