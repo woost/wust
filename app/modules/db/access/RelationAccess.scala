bf70985c7024a7db417e2937316a4e723bcb0de1
johannes karoff <johannes@karoff.net>
2015-06-28 01:43:32 +0200

    add todo

--- app/modules/db/access/RelationAccess.scala
+++ app/modules/db/access/RelationAccess.scala
@@ -7,6 +7,7 @@ import play.api.libs.json.JsValue
 import renesca.schema._
 
 trait RelationAccess[-NODE <: UuidNode, +OTHER <: UuidNode] {
+  //TODO: switch left/right...left should be errors
   def read(baseDef: FixedNodeDefinition[NODE]): Either[Iterable[OTHER], String] = Right("No read access on Relation")
   def delete(baseDef: FixedNodeDefinition[NODE], uuid: String): Either[Boolean, String] = Right("No delete access on Relation")
   def deleteHyper(baseDef: HyperNodeDefinitionBase[NODE with AbstractRelation[_, _]], uuid: String): Either[Boolean, String] = Right("No delete access on HyperRelation")
