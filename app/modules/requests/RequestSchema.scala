81ac33825faddf8c3441b2bacfa6328e1e82facd
johannes karoff <johannes@karoff.net>
2015-06-28 13:03:14 +0200

    overrides for simple db access in api definition dsl

--- app/modules/requests/RequestSchema.scala
+++ app/modules/requests/RequestSchema.scala
@@ -58,23 +58,39 @@ END <: UuidNode
 object dsl {
   object N {
     def apply[NODE <: UuidNode](factory: UuidNodeFactory[NODE], op: NodeAccess[NODE], connectSchemas: (String, UuidNodeFactory[NODE] => ConnectSchema[NODE])*): (String) => NodeSchema[NODE] = {
-      (path) => new NodeSchema(path, op, connectSchemas.toMap.mapValues(_(factory)))
+      path => NodeSchema(path, op, connectSchemas.toMap.mapValues(_(factory)))
     }
 
     def -->[
     START <: UuidNode,
     RELATION <: AbstractRelation[START, END],
     END <: UuidNode
-    ](op: UuidNodeFactory[START] => StartRelationAccess[START, RELATION, END]): (UuidNodeFactory[START]) => StartConnectSchema[START, RELATION, END] = {
-      (baseFactory) => new StartConnectSchema(op(baseFactory))
+    ](op: UuidNodeFactory[START] => StartRelationAccess[START, RELATION, END]): UuidNodeFactory[START] => StartConnectSchema[START, RELATION, END] = {
+      baseFactory => StartConnectSchema(op(baseFactory))
     }
 
     def <--[
     START <: UuidNode,
     RELATION <: AbstractRelation[START, END],
     END <: UuidNode
-    ](op: UuidNodeFactory[END] => EndRelationAccess[START, RELATION, END]): (UuidNodeFactory[END]) => EndConnectSchema[START, RELATION, END] = {
-      (baseFactory) => new EndConnectSchema(op(baseFactory))
+    ](op: UuidNodeFactory[END] => EndRelationAccess[START, RELATION, END]): UuidNodeFactory[END] => EndConnectSchema[START, RELATION, END] = {
+      baseFactory => EndConnectSchema(op(baseFactory))
+    }
+
+    def -->[
+    START <: UuidNode,
+    RELATION <: AbstractRelation[START, END],
+    END <: UuidNode
+    ](op: StartRelationAccess[START, RELATION, END]): UuidNodeFactory[START] => StartConnectSchema[START, RELATION, END] = {
+      baseFactory => StartConnectSchema(op)
+    }
+
+    def <--[
+    START <: UuidNode,
+    RELATION <: AbstractRelation[START, END],
+    END <: UuidNode
+    ](op: EndRelationAccess[START, RELATION, END]): UuidNodeFactory[END] => EndConnectSchema[START, RELATION, END] = {
+      baseFactory => EndConnectSchema(op)
     }
 
     def --->[
@@ -82,7 +98,7 @@ object dsl {
     RELATION <: AbstractRelation[START, END] with UuidNode,
     END <: UuidNode
     ](factory: AbstractRelationFactory[START, RELATION, END] with UuidNodeFactory[RELATION], op: UuidNodeFactory[START] => StartRelationAccess[START, RELATION, END], connectSchemas: (String, (UuidNodeFactory[RELATION]) => ConnectSchema[RELATION])*): UuidNodeFactory[START] => StartHyperConnectSchema[START, RELATION, END] = {
-      (baseFactory) => new StartHyperConnectSchema(factory, op(baseFactory), connectSchemas.toMap.mapValues(_(factory)))
+      (baseFactory) => StartHyperConnectSchema(factory, op(baseFactory), connectSchemas.toMap.mapValues(_(factory)))
     }
 
     def <---[
@@ -90,7 +106,23 @@ object dsl {
     RELATION <: AbstractRelation[START, END] with UuidNode,
     END <: UuidNode
     ](factory: AbstractRelationFactory[START, RELATION, END] with UuidNodeFactory[RELATION], op: UuidNodeFactory[END] => EndRelationAccess[START, RELATION, END], connectSchemas: (String, (UuidNodeFactory[RELATION]) => ConnectSchema[RELATION])*): UuidNodeFactory[END] => EndHyperConnectSchema[START, RELATION, END] = {
-      (baseFactory) => new EndHyperConnectSchema(factory, op(baseFactory), connectSchemas.toMap.mapValues(_(factory)))
+      baseFactory => EndHyperConnectSchema(factory, op(baseFactory), connectSchemas.toMap.mapValues(_(factory)))
+    }
+
+    def --->[
+    START <: UuidNode,
+    RELATION <: AbstractRelation[START, END] with UuidNode,
+    END <: UuidNode
+    ](factory: AbstractRelationFactory[START, RELATION, END] with UuidNodeFactory[RELATION], op: StartRelationAccess[START, RELATION, END], connectSchemas: (String, (UuidNodeFactory[RELATION]) => ConnectSchema[RELATION])*): UuidNodeFactory[START] => StartHyperConnectSchema[START, RELATION, END] = {
+      baseFactory => StartHyperConnectSchema(factory, op, connectSchemas.toMap.mapValues(_(factory)))
+    }
+
+    def <---[
+    START <: UuidNode,
+    RELATION <: AbstractRelation[START, END] with UuidNode,
+    END <: UuidNode
+    ](factory: AbstractRelationFactory[START, RELATION, END] with UuidNodeFactory[RELATION], op: EndRelationAccess[START, RELATION, END], connectSchemas: (String, (UuidNodeFactory[RELATION]) => ConnectSchema[RELATION])*): UuidNodeFactory[END] => EndHyperConnectSchema[START, RELATION, END] = {
+      baseFactory => EndHyperConnectSchema(factory, op, connectSchemas.toMap.mapValues(_(factory)))
     }
   }
 }
