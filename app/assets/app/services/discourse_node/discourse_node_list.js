963afdaa764d5f5ed1152875fa336f71a636da84
johannes karoff <johannes@karoff.net>
2015-07-08 16:21:47 +0200

    remove subscription logic from discourse_node_list

--- app/assets/app/services/discourse_node/discourse_node_list.js
+++ app/assets/app/services/discourse_node/discourse_node_list.js
@@ -32,7 +32,6 @@ function DiscourseNodeList() {
             };
         }
 
-
         class NodeModel {
             constructor(nodeList, title, listCss) {
                 this.list = nodeList;
@@ -42,7 +41,6 @@ function DiscourseNodeList() {
                 this.nestedNodeLists = [];
 
                 //TODO: assure that the list is a restmod collection?
-                // same for subscribe...
                 if (this.list.$on !== undefined) {
                     // will create nested NodeLists for each added node
                     this.list.$on("after-add", (node) => {
@@ -59,66 +57,12 @@ function DiscourseNodeList() {
                 });
             }
 
-            removeLocally(id) {
-                let elem = _.find(this.list, {
-                    id: id
-                });
-
-                this.list.$remove(elem);
-            }
-
-            addLocally(elem) {
-                if (this.exists(elem))
-                    return;
-
-                this.list.$buildRaw(elem).$reveal();
-            }
-
             nested(nodeListCreate, servicePath, title) {
                 this.isNested = true;
                 this.nestedNodeLists.push({
                     servicePath,
                     create: service => nodeListCreate(service.$search(), title)
                 });
-            }
-
-            subscribe(unsubscribe) {
-                if (this.list.$subscribeToLiveEvent === undefined) {
-                    console.warn("Cannot subscribe non-schema collection");
-                    return _.noop;
-                }
-
-                let onConnectionChange = (list, message) => $rootScope.$apply(() => {
-                    switch (message.type) {
-                        case "connect":
-                            list.addLocally(message.data);
-                            break;
-                        case "disconnect":
-                            list.removeLocally(message.data.id);
-                            break;
-                        default:
-                    }
-                });
-
-                let unsubscribeFuncs = _.map(this.nestedNodeLists, (list, i) => this.list.$subscribeToLiveEvent(m => {
-                    let node = _.find(this.list, {id: m.reference});
-                    if (node !== undefined) {
-                        let nestedList = node.nestedNodeLists[i];
-                        onConnectionChange(nestedList.model, m);
-                    }
-                }, list.servicePath));
-
-                unsubscribeFuncs.push(this.list.$subscribeToLiveEvent(m => onConnectionChange(this, m)));
-
-                let unsubscribeFunc = () => _.each(unsubscribeFuncs, func => func());
-                if (unsubscribe) {
-                    let deregisterEvent = $rootScope.$on("$stateChangeSuccess", () => {
-                        unsubscribeFunc();
-                        deregisterEvent();
-                    });
-                }
-
-                return unsubscribeFunc;
             }
         }
 
@@ -165,10 +109,6 @@ function DiscourseNodeList() {
             nested(nodeListCreate, servicePath, title) {
                 this.model.nested(nodeListCreate, servicePath, title);
                 return this;
-            }
-
-            subscribe(unsubscribe = true) {
-                return this.model.subscribe(unsubscribe);
             }
         }
 
