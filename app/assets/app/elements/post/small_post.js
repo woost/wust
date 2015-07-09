1188a64142e5e50b15075a33e0c6dbc388cfa480
johannes karoff <johannes@karoff.net>
2015-07-09 13:48:22 +0200

    multiple drop channels for moving in scratchpad and connecting to other node
    - e.g. local nodes without a title cannot be connected (missing title)

--- app/assets/app/elements/post/small_post.js
+++ app/assets/app/elements/post/small_post.js
@@ -8,6 +8,7 @@ function smallPost() {
         templateUrl: "assets/app/elements/post/small_post.html",
         scope: {
             node: "=",
+            valid: "="
         },
         controller: smallPostCtrl,
         controllerAs: "vm",
@@ -20,5 +21,6 @@ smallPostCtrl.$inject = ["DiscourseNode"];
 function smallPostCtrl(DiscourseNode) {
     let vm = this;
 
+    vm.valid = vm.valid === undefined ? true : vm.valid;
     vm.nodeInfo = DiscourseNode.Post;
 }
