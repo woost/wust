14bc285ff437d26c57e12026c637ce5603cb04f1
johannes karoff <johannes@karoff.net>
2015-07-24 14:32:33 +0200

    go back to compiling the popover by ourself, using $popover service does not work for content-template

--- app/assets/app/elements/edit_popover/edit_popover.js
+++ app/assets/app/elements/edit_popover/edit_popover.js
@@ -1,21 +1,26 @@
 angular.module("wust.elements").directive("editPopover", editPopover);
 
-editPopover.$inject = ["$popover"];
+editPopover.$inject = ["$compile"];
 
-function editPopover($popover) {
+function editPopover($compile) {
     return {
         priority: 1001, // compiles first
         terminal: true, // prevent lower priority directives to compile after it
         restrict: "A",
         scope: true,
-        link: function(scope, el) {
-            var myPopover = $popover(el, {
-                "content-template": "assets/app/elements/edit_popover/edit_popover.html",
-                "title": "penos",
-                "placement": "bottom",
-                "auto-close": "1",
-                "animation": "am-flip-x",
-            });
+        compile: function(el) {
+            el.removeAttr("edit-popover"); // necessary to avoid in
+            el.attr("content-template", "assets/app/elements/edit_popover/edit_popover.html");
+            el.attr("title", "penos");
+            el.attr("placement", "bottom");
+            el.attr("auto-close", "1");
+            el.attr("animation", "am-flip-x");
+            el.attr("bs-popover", "");
+            el[0].classList.add("edit_popover_directive");
+            var fn = $compile(el);
+            return function(scope) {
+                fn(scope);
+            };
         }
     };
 }
