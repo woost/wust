3ae9ea0a934fdc4312ea717e4ae2b483b4379dd7
johannes karoff <johannes@karoff.net>
2015-08-05 03:27:59 +0200

    drop on left side toggle button

--- app/assets/app/components/page/page_ctrl.js
+++ app/assets/app/components/page/page_ctrl.js
@@ -1,9 +1,15 @@
 angular.module("wust.components").controller("PageCtrl", PageCtrl);
 
-PageCtrl.$inject = ["LeftSideService"];
+PageCtrl.$inject = ["LeftSideService", "EditService"];
 
-function PageCtrl(LeftSideService) {
+function PageCtrl(LeftSideService, EditService) {
     let vm = this;
 
     vm.leftSide = LeftSideService;
+    vm.editNode = editNode;
+
+    function editNode(data) {
+        LeftSideService.visible = true;
+        EditService.edit(data);
+    }
 }
