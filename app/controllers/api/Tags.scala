35b27af275ff93661a280756d8900d80fa219925
johannes karoff <johannes@karoff.net>
2015-08-12 22:17:51 +0200

    tagged taggable for tag api posts

--- app/controllers/api/Tags.scala
+++ app/controllers/api/Tags.scala
@@ -3,11 +3,11 @@ package controllers.api
 import controllers.api.nodes.Nodes
 import model.WustSchema._
 import modules.db.access.StartRelationRead
-import modules.db.access.custom.TagAccess
+import modules.db.access.custom.{TagAccess, TaggedTaggable}
 import modules.requests.dsl._
 
 object Tags extends Nodes[TagLike] {
   val node = NodeDef(TagLikeMatches, TagAccess.apply,
-    ("posts", N > StartRelationRead(Categorizes, Post))
+    ("posts", N > StartRelationRead(Categorizes, Post) + TaggedTaggable.apply[Taggable])
   )
 }
