cedc59324515bff33adcffee76b455581cce7d38
johannes karoff <johannes@karoff.net>
2015-08-17 01:33:05 +0200

    big fat post

--- app/assets/app/components/focus/neighbours/focus_neighbours.js
+++ app/assets/app/components/focus/neighbours/focus_neighbours.js
@@ -20,8 +20,8 @@ NeighboursCtrl.$inject = ["Post", "DiscourseNodeList"];
 function NeighboursCtrl(Post, DiscourseNodeList) {
     let vm = this;
 
-    vm.references = DiscourseNodeList.write.Post.successors(vm.component, vm.component.rootNode, "connectsTo")
-        .nested(DiscourseNodeList.write.Post.predecessors, "connectsFrom");
-    vm.replies = DiscourseNodeList.write.Post.predecessors(vm.component, vm.component.rootNode, "connectsFrom")
-        .nested(DiscourseNodeList.write.Post.predecessors, "connectsFrom");
+    vm.references = DiscourseNodeList.write.Post.successors(vm.component, vm.component.rootNode, "connectsTo");
+        // .nested(DiscourseNodeList.write.Post.predecessors, "connectsFrom");
+    vm.replies = DiscourseNodeList.write.Post.predecessors(vm.component, vm.component.rootNode, "connectsFrom");
+        // .nested(DiscourseNodeList.write.Post.predecessors, "connectsFrom");
 }
