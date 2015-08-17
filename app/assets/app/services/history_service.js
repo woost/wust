7397e495b17765568f1929be73a62e5948d540d9
Felix Dietze <felix@beerleader.net>
2015-08-17 15:53:08 +0200

    tag circles in history

--- app/assets/app/services/history_service.js
+++ app/assets/app/services/history_service.js
@@ -72,7 +72,6 @@ function HistoryService(Post, DiscourseNode, store) {
     }
 
     function addNode(node) {
-        node = node.encode ? node.encode() : node;
         _.remove(self.visited, n => node.id === n.id);
         self.visited.push(node);
         self.visited.splice(0, self.visited.length - maximum);
