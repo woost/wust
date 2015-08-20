74553e1888458caedc3c44ff509c526c3ea6f97c
johannes karoff <johannes@karoff.net>
2015-08-20 16:31:34 +0200

    deactive history service restoring, to reduce traffic for now

--- app/assets/app/services/history_service.js
+++ app/assets/app/services/history_service.js
@@ -50,11 +50,11 @@ function HistoryService(Post, DiscourseNode, store) {
     }
 
     function restoreNode(id) {
-        Post.$find(id).$then(node => {
-            let encoded = node.$encode();
-            encoded.tags = node.tags.$encode();
-            addNode(encoded);
-        });
+        // Post.$find(id).$then(node => {
+        //     let encoded = node.$encode();
+        //     encoded.tags = node.tags.$encode();
+        //     addNode(encoded);
+        // });
     }
 
     function storeVisited() {
