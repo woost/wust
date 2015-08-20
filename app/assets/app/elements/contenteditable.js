b8e4e6d13b92519f101c90df94f0dc597d03b9e6
johannes karoff <johannes@karoff.net>
2015-08-20 18:17:11 +0200

    handle maximum number of chars in contenteditable

--- app/assets/app/elements/contenteditable.js
+++ app/assets/app/elements/contenteditable.js
@@ -6,18 +6,36 @@ function contenteditable() {
   return {
     require: "ngModel",
     link: function(scope, element, attrs, ngModel) {
+        //TODO: does not handle copy paste
+        if (attrs.maxchars) {
+            element.bind("keydown", function(e) {
+                var keycode = e.keyCode;
 
-      function read() {
-        ngModel.$setViewValue(element.html());
-      }
+                //List of keycodes of printable characters from:
+                //http://stackoverflow.com/questions/12467240/determine-if-javascript-e-keycode-is-a-printable-non-control-character
+                var printable =
+                    (keycode > 47 && keycode < 58)   || // number keys
+                    keycode === 32 || keycode === 13   || // spacebar & return key(s) (if you want to allow carriage returns)
+                    (keycode > 64 && keycode < 91)   || // letter keys
+                    (keycode > 95 && keycode < 112)  || // numpad keys
+                    (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
+                    (keycode > 218 && keycode < 223);   // [\]' (in order)
 
-      ngModel.$render = function() {
-        element.html(ngModel.$viewValue || "");
-      };
+                let text = element.text();
+                if (printable) {
+                    if (text.length >= attrs.maxchars) {
+                        e.preventDefault();
+                        return;
+                    }
+                }
 
-      element.bind("blur keyup change", function() {
-        scope.$apply(read);
-      });
+                scope.$apply(() => ngModel.$setViewValue(text));
+            });
+        }
+
+        ngModel.$render = function() {
+            element.text(ngModel.$viewValue || "");
+        };
     }
   };
 }
