2dbd836e5e2a025b19f4e2ac2f38bfe40f1ddb08
Felix Dietze <felix@beerleader.net>
2015-08-21 20:29:54 +0200

    concat js and css

--- Brocfile.js
+++ Brocfile.js
@@ -1,21 +1,45 @@
-var compileSass = require('broccoli-sass');
-var esTranspiler = require('broccoli-babel-transpiler');
-var mergeTrees = require('broccoli-merge-trees');
-var compileSass = require('broccoli-compass');
-var funnel = require('broccoli-funnel');
+var mergeTrees = require("broccoli-merge-trees");
+var funnel = require("broccoli-funnel");
+var concat = require("broccoli-concat");
+
+var esTranspiler = require("broccoli-babel-transpiler");
+
+var compileSass = require("broccoli-compass");
+var cleanCSS = require("broccoli-clean-css");
+
+
+
+//TODO: jshint
+//TODO: asset fingerprinting
+//TODO: css minify
 
 var stylesTree = mergeTrees([
-        funnel('app/assets/stylesheets', { include: ['*.scss']}),
-        funnel('app/assets/app', { include: ['**/*.scss'] })
-                                                  ]);
+    funnel("app/assets/stylesheets", { include: ["*.scss"] }),
+    funnel("app/assets/app", { include: ["**/*.scss"], destDir: "app"
+    })
+]);
 
-var styles = compileSass(stylesTree, {
-  outputStyle: 'expanded',
-  sassDir: '.',
-})
+var compiledStyles = compileSass(stylesTree, {
+    outputStyle: "expanded",
+    sassDir: ".",
+});
+
+var styles = concat(compiledStyles, {
+    inputFiles: ["stylesheets/**/*.css"],
+    outputFile: "/main.css"
+});
 
 
-var scripts = esTranspiler(funnel('app/assets/app', { include: ['**/*.js']}));
+var compiledScripts = esTranspiler(funnel("app/assets/app", {
+    include: ["**/*.js"],
+    destDir: "javascripts"
+}));
+
+var scripts = concat(compiledScripts, {
+    inputFiles: ["javascripts/**/*.js"],
+    outputFile: "/main.js",
+    wrapInFunction: true
+});
 
 // Merge the compiled styles and scripts into one output directory.
 module.exports = mergeTrees([styles, scripts]);
