e1cd3c84c364a2b14ff3e20bd777827767e06264
Felix Dietze <felix@beerleader.net>
2015-09-20 17:30:29 +0200

    fix linting error

--- assets/debug/error.js
+++ assets/debug/error.js
@@ -1,6 +1,6 @@
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
         // console.log(xmlHttp.responseText);
         var responses = JSON.parse(xmlHttp.responseText);
         if (document.readyState === "interactive" || document.readyState === "complete")
@@ -8,48 +8,49 @@ xmlHttp.onreadystatechange = function() {
         else
             document.addEventListener("DOMContentLoaded", setErrorMessage);
 
-        function setErrorMessage() {
-            var parentElem = document.createElement("div");
-            Object.keys(responses).forEach(function(reporter) {
-                var response = responses[reporter];
-                if (!response.errors || !response.errors.length)
-                    return;
+    }
 
-                var errorElem = document.createElement("pre");
-                errorElem.style.padding = "20px";
-                errorElem.style.margin = "10px";
-                var errorHead = document.createElement("h2");
-                errorHead.appendChild(document.createTextNode(reporter + " reported:"));
-                errorElem.appendChild(errorHead);
-                response.errors.forEach(function(error) {
-                    var elem = document.createElement("div");
-                    var formattedError = error
-                        .replace(/\u001b\[31m/g,"<span style='color:red'>")
-                        .replace(/\u001b\[32m/g,"<span style='color:green'>")
-                        .replace(/\u001b\[33m/g,"<span style='color:yellow'>")
-                        .replace(/\u001b\[34m/g,"<span style='color:blue'>")
-                        .replace(/\u001b\[35m/g,"<span style='color:magenta'>")
-                        .replace(/\u001b\[36m/g,"<span style='color:cyan'>")
-                        .replace(/\u001b\[37m/g,"<span style='color:white'>")
-                        .replace(/\u001b\[39m/g,"</span>") // foreground default
-                        .replace(/\u001b\[0m/g,"</span>"); // reset all attributes
-                    var formattedElem = document.createElement("span");
-                    elem.innerHTML = formattedError;
-                    errorElem.appendChild(elem);
-                });
+    function setErrorMessage() {
+        var parentElem = document.createElement("div");
+        Object.keys(responses).forEach(function(reporter) {
+            var response = responses[reporter];
+            if (!response.errors || !response.errors.length)
+                return;
 
-                var errorSumElem = document.createElement("div");
-                errorSumElem.style.paddingTop = "10px";
-                errorSumElem.style.fontWeight = "bold";
-                errorSumElem.appendChild(document.createTextNode(response.errors.length + " errors"));
-                errorElem.appendChild(errorSumElem);
-                parentElem.appendChild(errorElem);
+            var errorElem = document.createElement("pre");
+            errorElem.style.padding = "20px";
+            errorElem.style.margin = "10px";
+            var errorHead = document.createElement("h2");
+            errorHead.appendChild(document.createTextNode(reporter + " reported:"));
+            errorElem.appendChild(errorHead);
+            response.errors.forEach(function(error) {
+                var elem = document.createElement("div");
+                var formattedError = error
+                    .replace(/\u001b\[31m/g, "<span style='color:red'>")
+                    .replace(/\u001b\[32m/g, "<span style='color:green'>")
+                    .replace(/\u001b\[33m/g, "<span style='color:yellow'>")
+                    .replace(/\u001b\[34m/g, "<span style='color:blue'>")
+                    .replace(/\u001b\[35m/g, "<span style='color:magenta'>")
+                    .replace(/\u001b\[36m/g, "<span style='color:cyan'>")
+                    .replace(/\u001b\[37m/g, "<span style='color:white'>")
+                    .replace(/\u001b\[39m/g, "</span>") // foreground default
+                    .replace(/\u001b\[0m/g, "</span>"); // reset all attributes
+                var formattedElem = document.createElement("span");
+                elem.innerHTML = formattedError;
+                errorElem.appendChild(elem);
             });
 
-            if (parentElem.childElementCount) {
-                document.body.innerHTML = parentElem.outerHTML;
-                document.body.style.overflow = "auto";
-            }
+            var errorSumElem = document.createElement("div");
+            errorSumElem.style.paddingTop = "10px";
+            errorSumElem.style.fontWeight = "bold";
+            errorSumElem.appendChild(document.createTextNode(response.errors.length + " errors"));
+            errorElem.appendChild(errorSumElem);
+            parentElem.appendChild(errorElem);
+        });
+
+        if (parentElem.childElementCount) {
+            document.body.innerHTML = parentElem.outerHTML;
+            document.body.style.overflow = "auto";
         }
     }
 };
