b58ea7ccca0c0cb8249a72a7d161a3952df70216
Felix Dietze <felix@beerleader.net>
2015-09-21 20:48:41 +0200

    show karma next to contexts in nav

--- assets/app/services/context_service.js
+++ assets/app/services/context_service.js
@@ -1,14 +1,26 @@
 angular.module("wust.services").service("ContextService", ContextService);
 
-ContextService.$inject = ["$rootScope", "Helpers"];
+ContextService.$inject = ["$rootScope", "Helpers", "User", "Auth"];
 
-function ContextService($rootScope, Helpers) {
+function ContextService($rootScope, Helpers, User, Auth) {
     let self = this;
 
     this.currentContexts = [];
     this.emitChangedEvent = emitChangedEvent;
+    this.updateKarma = updateKarma;
     this.setContext = setContext;
     this.contextStyle = {};
+
+    function updateKarma() {
+        if( Auth.current.userId ) {
+            User.$buildRaw({id: Auth.current.userId}).karma.$search().$then(response =>
+                    response.forEach(k => {
+                        let context =  _.find(this.currentContexts, c => c.id === k.id);
+                        if( context )
+                            context.karma = k.karma;
+                    }));
+        }
+    }
 
     function setContext(node) {
         let firstContext = Helpers.sortByIdQuality(node.tags)[0];
@@ -20,6 +32,7 @@ function ContextService($rootScope, Helpers) {
     }
 
     function emitChangedEvent() {
+        this.updateKarma();
         this.contextStyle["background-color"] = this.currentContexts.length > 0 ? Helpers.navBackgroundColor(this.currentContexts[0]) : undefined;
         this.contextStyle["border-bottom"] = this.currentContexts.length > 0 ? ("1px solid " + Helpers.contextCircleBorderColor(this.currentContexts[0])) : undefined;
         $rootScope.$emit("context.changed");
