8f1965fe9a3b92e3729074c3ec11a1c76976d917
johannes karoff <johannes@karoff.net>
2015-09-30 23:57:07 +0200

    move marked config from directive into config file

---
+++ assets/app/config/markdown.js
@@ -0,0 +1,16 @@
+angular.module("wust.config").config(HumaneConfig);
+
+HumaneConfig.$inject = [];
+
+function HumaneConfig() {
+    marked.setOptions({
+        renderer: new marked.Renderer(),
+        gfm: true,
+        tables: true,
+        breaks: false,
+        pedantic: false,
+        sanitize: true, // additionally escape all input html elements
+        smartLists: true,
+        smartypants: false
+    });
+}
--- assets/app/elements/markdown.js
+++ assets/app/elements/markdown.js
@@ -3,17 +3,6 @@ angular.module("wust.elements").directive("markdown", markdown);
 markdown.$inject = [];
 
 function markdown() {
-    marked.setOptions({
-        renderer: new marked.Renderer(),
-        gfm: true,
-        tables: true,
-        breaks: false,
-        pedantic: false,
-        sanitize: false, // additionally escape all input html elements
-        smartLists: true,
-        smartypants: false
-    });
-
     // ng-bind-html internally checks whether the html was explicitly trusted,
     // if not it checks whether ngSanitize is available and sanitizes the html
     // (otherwise an error is raised). We did not trust the output of the markdown
