698536c85846766ccf8d88087ad7d0b20d5629ef
johannes karoff <johannes@karoff.net>
2015-10-02 12:27:44 +0200

    remove outdated todo in assets

--- assets/app/elements/node_list/discourse_node_list.js
+++ assets/app/elements/node_list/discourse_node_list.js
@@ -39,7 +39,6 @@ function discourseNodeListCtrl(Post, TagRelationEditService, Auth) {
         deregisterCommit();
     });
 
-    //TODO: unvote
     function upvoteAnswer(connectable) {
         let connects = findConnects(vm.nodeModel.component.rootNode, connectable);
         vm.connectsRelations[connectable.id] = connects;
