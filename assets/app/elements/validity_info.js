df30c2a5cce8cdd88bbed9369ac9c2c479005b5b
johannes karoff <johannes@karoff.net>
2015-10-03 23:06:44 +0200

    spacing new_post directive

--- assets/app/elements/validity_info.js
+++ assets/app/elements/validity_info.js
@@ -5,6 +5,7 @@ validityInfo.$inject = [];
 function validityInfo() {
     return {
         restrict: "A",
+        replace: true,
         template: "<div class='error_message' ng-class='{ show: validityInfo.triedSave && !validityInfo.validity[property].valid}' ng-bind='validityInfo.validity[property].message'></div>",
         scope: {
             validityInfo: "=",
