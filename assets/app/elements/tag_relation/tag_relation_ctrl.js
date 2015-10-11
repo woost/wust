1f0572694885c12c4d9ea3f6123e239e3625519b
johannes karoff <johannes@karoff.net>
2015-10-11 21:01:07 +0200

    show whether enough karma for edit in tag relation modal

--- assets/app/elements/tag_relation/tag_relation_ctrl.js
+++ assets/app/elements/tag_relation/tag_relation_ctrl.js
@@ -1,14 +1,24 @@
 angular.module("wust.elements").controller("TagRelationCtrl", TagRelationCtrl);
 
-TagRelationCtrl.$inject = ["TagRelationEditService"];
+TagRelationCtrl.$inject = ["$scope", "TagRelationEditService", "Auth", "KarmaService"];
 
-function TagRelationCtrl(TagRelationEditService) {
+function TagRelationCtrl($scope, TagRelationEditService, Auth, KarmaService) {
     let vm = this;
 
     vm.save = save;
     vm.cancel = cancel;
     vm.currentEditRelation = TagRelationEditService.currentRelation;
     vm.disconnect = TagRelationEditService.disconnect;
+
+    let authorBoost = Auth.current.userId === vm.currentEditRelation.startNode.author.id ? wust.Moderation().authorKarmaBoost : 0;
+    $scope.$on("component.changed", calculateEditWeight);
+    $scope.$on("karma.changed", calculateEditWeight);
+    calculateEditWeight();
+
+    function calculateEditWeight() {
+        vm.editWeight = KarmaService.voteWeightInContexts(vm.currentEditRelation.startNode.connectedTags) + authorBoost;
+        vm.canApply = wust.Moderation().canApply(vm.editWeight, vm.currentEditRelation.startNode.postChangeThreshold);
+    }
 
     function cancel() {
         if( vm.currentEditRelation.isCreating )
