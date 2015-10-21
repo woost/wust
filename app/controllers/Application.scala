804718e2c6aa86ac88c87b84e35eff52d509c7e2
johannes karoff <johannes@karoff.net>
2015-10-21 13:08:11 +0200

    introduce variables for branding + survey

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,6 +17,10 @@ object Application extends Controller {
   private val nodeSchemasJson = Json.toJson(JsArray(nodeSchemas.map(Json.toJson(_))));
 
   val scratchpadEnabled = "ui.scratchpad.enabled".configOrElse(true)
+  val brandingText = "ui.branding.text".configOrElse("")
+  val brandingLogo = "ui.branding.logo".configOrElse("")
+  val brandingColor = "ui.branding.color".configOrElse("")
+  val surveyEnabled = "ui.survey.enabled".configOrElse(false)
 
   def index(any: String) = Action {
     Ok(views.html.index(
@@ -25,7 +29,11 @@ object Application extends Controller {
         ("models", nodeSchemasJson)
       )),
       JsObject(Seq(
-        ("scratchpadEnabled", JsBoolean(scratchpadEnabled))
+        ("scratchpadEnabled", JsBoolean(scratchpadEnabled)),
+        ("brandingText", JsString(brandingText)),
+        ("brandingLogo", JsString(brandingLogo)),
+        ("brandingColor", JsString(brandingColor)),
+        ("surveyEnabled", JsBoolean(surveyEnabled))
       ))
     ))
   }
