961fded1be2293f6d384c75eac6d5dd84ef5ecdf
johannes karoff <johannes@karoff.net>
2015-10-21 14:55:39 +0200

    add minimal survey overlay

---
+++ assets/app/elements/survey/survey.js
@@ -0,0 +1,58 @@
+angular.module("wust.elements").directive("survey", survey);
+
+survey.$inject = [];
+
+function survey() {
+    return {
+        restrict: "A",
+        replace: true,
+        templateUrl: "elements/survey/survey.html",
+        scope: true,
+        controller: surveyCtrl,
+        controllerAs: "vm",
+        bindToController: true
+    };
+}
+
+surveyCtrl.$inject = ["store"];
+
+function surveyCtrl(store) {
+    let vm = this;
+    let surveyStore = store.getNamespacedStore("survey");
+
+    vm.currentIndex = surveyStore.get("currentIndex") || 0;
+    vm.exercises = [
+        {
+            title: "Do something",
+            description: "Should be a cool thing"
+        },
+        {
+            title: "Vielen Dank für deine Teilnahme!",
+            description: "Bitte fülle jetzt den Fragebogen aus"
+        }
+    ];
+
+    vm.next = next;
+    vm.back = back;
+    vm.finished = finished;
+
+    function finished() {
+        surveyStore.set("currentIndex", undefined);
+    }
+
+    function back() {
+        if (vm.currentIndex < 1)
+            return;
+
+        vm.currentIndex--;
+        surveyStore.set("currentIndex", vm.currentIndex);
+    }
+
+    function next() {
+        if (vm.currentIndex >= vm.exercises.length - 1)
+            return;
+
+        vm.currentIndex++;
+        surveyStore.set("currentIndex", vm.currentIndex);
+    }
+}
