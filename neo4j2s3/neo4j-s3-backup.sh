e2a57dfdb0709ed4140ca25a6ca21f417f5fd510
johannes karoff <johannes@karoff.net>
2016-10-12 22:40:44 +0200

    double .cql

--- neo4j2s3/neo4j-s3-backup.sh
+++ neo4j2s3/neo4j-s3-backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-backup_file="$(date +"%Y-%m-%d-%H-%M-%S-backup.cql").cql"
+backup_file="$(date +"%Y-%m-%d-%H-%M-%S-backup.cql")"
 /var/lib/neo4j/bin/neo4j-shell -host neo4j -c dump > $backup_file
 
 aws s3 cp "$backup_file" "s3://wust-fliegenpilz/backup/$backup_file"
